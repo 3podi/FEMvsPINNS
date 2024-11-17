49ab07706bc9f71618fc0c09a47474ffc9b0e979
3podi <riccardo1.tripodi@mail.polimi.it>
2024-11-17 13:00:32 +0100

    fix shape sol gt

--- FEM_codes/1D_Schroedinger_ground_truth.py
+++ FEM_codes/1D_Schroedinger_ground_truth.py
@@ -33,9 +33,6 @@ indices = np.random.choice(range(len(all_times)), size=dt_coords_size, replace=F
 indices = np.sort(indices) + 1
 print('Timesteps saved for GT: ', indices)
 saved_times = np.array(all_times)[indices-1]
-true_u = np.zeros((dt_coords_size,nums[0]))
-true_v = np.zeros((dt_coords_size,nums[0]))
-true_h = np.zeros((dt_coords_size,nums[0]))
 sol_matrix = []
 n_sol=0
 eval_coordinates = {}
@@ -48,6 +45,9 @@ for num in nums:
   print('Start solving', num)
   mesh = IntervalMesh(num,-5.0, 5.0) # Declare mesh
   eval_coordinates['mesh_coord']['0'] = mesh.coordinates().tolist()
+  true_u = np.zeros((dt_coords_size,len(eval_coordinates['mesh_coord']['0'])))
+  true_v = np.zeros((dt_coords_size,len(eval_coordinates['mesh_coord']['0'])))
+  true_h = np.zeros((dt_coords_size,len(eval_coordinates['mesh_coord']['0'])))
   V = VectorFunctionSpace(mesh, 'CG', 1, dim = 2, constrained_domain = pbc) # Periodic BC are included in the definition of the function space
   # CG is type of the finite element, and 1 is the degree
   # Here vector space is used because we must write separate equations for real and imaginary parts of h, and h is [h_re , h_im]
@@ -106,8 +106,8 @@ for num in nums:
             solutions_at_eval_points.append((u_eval[0], u_eval[1]))  # (Real, Imaginary)
 
           #solution_values = u.vector().get_local().reshape((-1, 2))
-          true_u[n_sol,:] = [sol[0] for sol in solutions_at_eval_points]
-          true_v[n_sol,:] = [sol[1] for sol in solutions_at_eval_points]
+          true_u[n_sol,:] = [sol[0] for sol in enumerate(solutions_at_eval_points)]
+          true_v[n_sol,:] = [sol[1] for sol in enumerate(solutions_at_eval_points)]
           true_h[n_sol,:] = np.sqrt(true_u[n_sol,:]**2 + true_v[n_sol,:]**2)
           n_sol += 1
 
